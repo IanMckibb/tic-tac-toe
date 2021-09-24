4f8fab5836a9303c4b2bfb99a997dd5230c6366b
Meeperbunny <mckibbenian@gmail.com>
2021-09-24 16:49:14 -0600

    Created display function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include <vector>
+#include <string>
 
 // Creates a 3 by 3 int vector and returns it
 std::vector<std::vector<int>> CreateBoard() {
@@ -10,11 +12,25 @@ std::vector<std::vector<int>> CreateBoard() {
         // Push empty vector
         return_vec.push_back(temp);
     }
-
     return return_vec;
+}
+
+// Display the board
+void DisplayBoard(std::vector<std::vector<int>> const &board) {
+    for(unsigned int i = 0; i < board.size(); i++) {
+        std::cout << ((i != 0) ? std::string(board[i].size() * 4 - 1, '-') + "\n" : "") << " ";
+        for(unsigned int q = 0; q < board[i].size(); q++) {
+            std::string display_str;
+            if (board[i][q] == 0) display_str = " ";
+            else if (board[i][q] == 1) display_str = "X";
+            else if (board[i][q] == 2) display_str = "O";
+            std::cout << display_str << ((q != board[i].size() - 1) ? " | " : "\n");
+        }
+    }
 }
 
 int main() {
     // Initalizes my_board using copy constructor
     std::vector<std::vector<int>> my_board(CreateBoard());
+    DisplayBoard(my_board);
 }
