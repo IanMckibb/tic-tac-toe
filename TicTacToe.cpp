a021eb8716cedda13b335b3a6d1d1e9bd2189347
WillM5462 <wimc5462@colorado.edu>
2021-09-24 16:41:38 -0600

    Added DisplayBoard function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 // Creates a 3 by 3 int vector and returns it
 std::vector<std::vector<int>> CreateBoard() {
@@ -12,6 +13,15 @@ std::vector<std::vector<int>> CreateBoard() {
     }
 
     return return_vec;
+}
+
+void DisplayBoard(std::vector<std::vector<int>> board){
+    for(int i = 0; i < 3; i++){
+        std::cout << " " << std::endl;
+        for(int j = 0; j < 3; j++){
+            std::cout << board[i][j] << " ";
+        }
+    }
 }
 
 int main() {
